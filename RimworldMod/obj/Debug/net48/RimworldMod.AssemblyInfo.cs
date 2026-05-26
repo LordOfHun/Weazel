da8e1c2c1c5e5635789cf742ecbeb571b84ef3cb
LordOfHun <38762989+LordOfHun@users.noreply.github.com>
2026-05-27 01:29:52 +0900

    05_27_새벽 작업

--- RimworldMod/obj/Debug/net48/RimworldMod.AssemblyInfo.cs
+++ RimworldMod/obj/Debug/net48/RimworldMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RimworldMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+028dfbaa4c8b16e304be1ca907d31abcae7ea6d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd5adc1ed956b1ca4a0a175bf6d588e743c1e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RimworldMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RimworldMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
